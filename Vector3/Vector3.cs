357e88de3ef05ae8a3f00b3f956e3a061dac2f75
ajbobo <ajb.codemonkey@gmail.com>
2022-10-30 12:30:34 -0600

    Initial version - Simple DLL with Test and Console app

---
+++ Vector3/Vector3.cs
@@ -0,0 +1,46 @@
+﻿namespace MSTest_Example
+{
+
+public class Vector3
+{
+    public double X { get; init; }
+    public double Y { get; init; }
+    public double Z { get; init; }
+
+    override public string ToString()
+    {
+        return $"<{X}, {Y}, {Z}>";
+    }
+
+    public Vector3 Add(Vector3 other)
+    {
+        return new Vector3
+        {
+            X = this.X + other.X,
+            Y = this.Y + other.Y,
+            Z = this.Z + other.Z
+        };
+    }
+
+    public double Dot(Vector3 other)
+    {
+        return (this.X * other.X) + (this.Y * other.Y) + (this.Z * other.Z);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        Vector3? other = obj as Vector3;
+        if (other == null)
+        {
+            return false;
+        }
+        
+        return (this.X == other.X && this.Y == other.Y && this.Z == other.Z);
+    }
+    
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+}
---
+++ Vector3_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MSTest_Example;
+
+public class ConsoleApp
+{
+    public static void Main(string[] args)
+    {
+        Vector3 v = new Vector3 { X = 1, Y = 2, Z = 3 };
+        Console.WriteLine($"V = {v}");
+
+        Vector3 v2 = new Vector3 { X = 2, Y = 2, Z = 2 };
+        Console.WriteLine($"V + V2 = {v.Add(v2)}");
+    }
+}
