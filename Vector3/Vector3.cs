96b5889149b5e3d44277374e93dce99a7326b44f
A.J. Bobo <aj.bobo@niceincontact.com>
2022-10-30 14:33:35 -0600

    Changed to .NET 5.0

--- Vector3/Vector3.cs
+++ Vector3/Vector3.cs
@@ -1,46 +1,45 @@
 ﻿namespace MSTest_Example
 {
-
-public class Vector3
-{
-    public double X { get; init; }
-    public double Y { get; init; }
-    public double Z { get; init; }
-
-    override public string ToString()
+    public class Vector3
     {
-        return $"<{X}, {Y}, {Z}>";
-    }
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
 
-    public Vector3 Add(Vector3 other)
-    {
-        return new Vector3
+        override public string ToString()
         {
-            X = this.X + other.X,
-            Y = this.Y + other.Y,
-            Z = this.Z + other.Z
-        };
-    }
-
-    public double Dot(Vector3 other)
-    {
-        return (this.X * other.X) + (this.Y * other.Y) + (this.Z * other.Z);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        Vector3? other = obj as Vector3;
-        if (other == null)
-        {
-            return false;
+            return $"<{X}, {Y}, {Z}>";
         }
-        
-        return (this.X == other.X && this.Y == other.Y && this.Z == other.Z);
+
+        public Vector3 Add(Vector3 other)
+        {
+            return new Vector3
+            {
+                X = this.X + other.X,
+                Y = this.Y + other.Y,
+                Z = this.Z + other.Z
+            };
+        }
+
+        public double Dot(Vector3 other)
+        {
+            return (this.X * other.X) + (this.Y * other.Y) + (this.Z * other.Z);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Vector3? other = obj as Vector3;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return (this.X == other.X && this.Y == other.Y && this.Z == other.Z);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
-    
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
-}
 }
--- Vector3_Test/Vector3_Tests.cs
+++ Vector3_Test/Vector3_Tests.cs
@@ -1,74 +1,74 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MSTest_Example;
-
-
-[TestClass]
-public class Vector3_Test
+namespace MSTest_Example
 {
-    [TestMethod]
-    public void TestToString()
+    [TestClass]
+    public class Vector3_Test
     {
-        Vector3 v = new Vector3 { X = 1, Y = 2, Z = 3 };
-        string vstr = v.ToString();
-        Assert.AreEqual<string>("<1, 2, 3>", vstr);
-    }
+        [TestMethod]
+        public void TestToString()
+        {
+            Vector3 v = new Vector3 { X = 1, Y = 2, Z = 3 };
+            string vstr = v.ToString();
+            Assert.AreEqual<string>("<1, 2, 3>", vstr);
+        }
 
-    [DataTestMethod]
-    [DataRow(1, 2, 3, "<1, 2, 3>")]
-    [DataRow(4, 4, 4, "<4, 4, 4>")]
-    [DataRow(1, 2, 3, "<1, 2, 3>")]
-    [DataRow(1.1, 2.2, 3.3, "<1.1, 2.2, 3.3>")]
-    [DataRow(1.1, 2, 3.3, "<1.1, 2, 3.3>")]
-    [DataRow(0, 2, 3.3, "<0, 2, 3.3>")]
-    [DataRow(0, 2, 3.345, "<0, 2, 3.345>")]
-    public void TestToString_Multiple(double x, double y, double z, string expected)
-    {
-        Vector3 v = new Vector3 { X = x, Y = y, Z = z };
-        string vstr = v.ToString();
-        Assert.AreEqual<string>(expected, vstr);
-    }
+        [DataTestMethod]
+        [DataRow(1, 2, 3, "<1, 2, 3>")]
+        [DataRow(4, 4, 4, "<4, 4, 4>")]
+        [DataRow(1, 2, 3, "<1, 2, 3>")]
+        [DataRow(1.1, 2.2, 3.3, "<1.1, 2.2, 3.3>")]
+        [DataRow(1.1, 2, 3.3, "<1.1, 2, 3.3>")]
+        [DataRow(0, 2, 3.3, "<0, 2, 3.3>")]
+        [DataRow(0, 2, 3.345, "<0, 2, 3.345>")]
+        public void TestToString_Multiple(double x, double y, double z, string expected)
+        {
+            Vector3 v = new Vector3 { X = x, Y = y, Z = z };
+            string vstr = v.ToString();
+            Assert.AreEqual<string>(expected, vstr);
+        }
 
-    [DataTestMethod]
-    [DataRow(1, 1, 1, 2, 2, 2, 3, 3, 3)]
-    [DataRow(1, 2, 3, 1, 2, 3, 2, 4, 6)]
-    [DataRow(1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 2.2, 4.4, 6.6)]
-    public void TestAdd(double x1, double y1, double z1,
-                        double x2, double y2, double z2,
-                        double ex, double ey, double ez)
-    {
-        Vector3 v1 = new Vector3 { X = x1, Y = y1, Z = z1};
-        Vector3 v2 = new Vector3 { X = x2, Y = y2, Z = z2};
-        Vector3 res = v1.Add(v2);
+        [DataTestMethod]
+        [DataRow(1, 1, 1, 2, 2, 2, 3, 3, 3)]
+        [DataRow(1, 2, 3, 1, 2, 3, 2, 4, 6)]
+        [DataRow(1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 2.2, 4.4, 6.6)]
+        public void TestAdd(double x1, double y1, double z1,
+                            double x2, double y2, double z2,
+                            double ex, double ey, double ez)
+        {
+            Vector3 v1 = new Vector3 { X = x1, Y = y1, Z = z1 };
+            Vector3 v2 = new Vector3 { X = x2, Y = y2, Z = z2 };
+            Vector3 res = v1.Add(v2);
 
-        Vector3 expected = new Vector3 { X = ex, Y = ey, Z = ez };
+            Vector3 expected = new Vector3 { X = ex, Y = ey, Z = ez };
 
-        Assert.AreEqual<Vector3>(expected, res);
-    }
+            Assert.AreEqual<Vector3>(expected, res);
+        }
 
-    [TestMethod]
-    public void TestEqualsNull()
-    {
-        Vector3 v = new Vector3 { X = 1, Y = 1, Z = 1};
+        [TestMethod]
+        public void TestEqualsNull()
+        {
+            Vector3 v = new Vector3 { X = 1, Y = 1, Z = 1 };
 
-        Assert.IsFalse(v == null);
-    }
+            Assert.IsFalse(v == null);
+        }
 
-    [TestMethod]
-    public void TestEquals()
-    {
-        Vector3 v1 = new Vector3 { X = 1, Y = 2, Z = 3};
-        Vector3 v2 = new Vector3 { X = 1, Y = 2, Z = 3};
+        [TestMethod]
+        public void TestEquals()
+        {
+            Vector3 v1 = new Vector3 { X = 1, Y = 2, Z = 3 };
+            Vector3 v2 = new Vector3 { X = 1, Y = 2, Z = 3 };
 
-        Assert.AreEqual(v1, v2);
-    }
+            Assert.AreEqual(v1, v2);
+        }
 
-    [TestMethod]
-    public void TestNotEquals()
-    {
-        Vector3 v1 = new Vector3 { X = 1, Y = 2, Z = 3};
-        Vector3 v2 = new Vector3 { X = 2, Y = 3, Z = 4};
+        [TestMethod]
+        public void TestNotEquals()
+        {
+            Vector3 v1 = new Vector3 { X = 1, Y = 2, Z = 3 };
+            Vector3 v2 = new Vector3 { X = 2, Y = 3, Z = 4 };
 
-        Assert.AreNotEqual(v1, v2);
+            Assert.AreNotEqual(v1, v2);
+        }
     }
 }
