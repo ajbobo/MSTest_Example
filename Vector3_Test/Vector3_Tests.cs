50b65bcaf7ae38ab3352b6842dbde5e1e9ee24de
ajbobo <ajb.codemonkey@gmail.com>
2022-10-30 13:48:35 -0600

    Added new tests Fixed TestAdd()

--- Vector3_Test/Vector3_Tests.cs
+++ Vector3_Test/Vector3_Tests.cs
@@ -18,6 +18,10 @@ public class Vector3_Test
     [DataRow(1, 2, 3, "<1, 2, 3>")]
     [DataRow(4, 4, 4, "<4, 4, 4>")]
     [DataRow(1, 2, 3, "<1, 2, 3>")]
+    [DataRow(1.1, 2.2, 3.3, "<1.1, 2.2, 3.3>")]
+    [DataRow(1.1, 2, 3.3, "<1.1, 2, 3.3>")]
+    [DataRow(0, 2, 3.3, "<0, 2, 3.3>")]
+    [DataRow(0, 2, 3.345, "<0, 2, 3.345>")]
     public void TestToString_Multiple(double x, double y, double z, string expected)
     {
         Vector3 v = new Vector3 { X = x, Y = y, Z = z };
@@ -27,12 +31,13 @@ public class Vector3_Test
 
     [DataTestMethod]
     [DataRow(1, 1, 1, 2, 2, 2, 3, 3, 3)]
+    [DataRow(1, 2, 3, 1, 2, 3, 2, 4, 6)]
     public void TestAdd(double x1, double y1, double z1,
                         double x2, double y2, double z2,
                         double ex, double ey, double ez)
     {
         Vector3 v1 = new Vector3 { X = x1, Y = y1, Z = z1};
-        Vector3 v2 = new Vector3 { X = x2, Y = y2, Z = y2};
+        Vector3 v2 = new Vector3 { X = x2, Y = y2, Z = z2};
         Vector3 res = v1.Add(v2);
 
         Vector3 expected = new Vector3 { X = ex, Y = ey, Z = ez };
